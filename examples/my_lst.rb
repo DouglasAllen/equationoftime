767aee7557db18bf7c84e69a8ccc239247d01e0a
Douglas Allen <kb9agt@gmail.com>
2014-09-25 18:22:42 -0500

    adding some examples back

---
+++ examples/my_lst.rb
@@ -0,0 +1,8 @@
+require 'eot'
+eot = Eot.new
+puts 'My Local Apparent Sidereal Time'
+loop do
+  eot.ajd = DateTime.now.to_time.utc.to_datetime.ajd
+  puts "LST = #{ eot.string_time(((eot.tl_aries * Eot::R2D) + -88.75) / 15.0) }"
+  sleep ( 1 - 0.0027379093499 / 1.002739093499) / 1.0027379093499
+end
