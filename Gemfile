12cc20a172da488d71becc3ea71a8b51ef5b5e65
Douglas Allen <kb9agt@gmail.com>
2014-09-26 00:38:30 -0500

    my_lst.rb is so close to sync milliseconds

--- Gemfile
+++ Gemfile
@@ -1,10 +1,10 @@
 source 'https://rubygems.org'
 ruby '2.1.2'
-# Specify your gem's dependencies in equationoftime.gemspec
+
 gemspec
-gem 'addressable'
+
 group :development do
-  gem 'robocop'
+  gem 'rubocop'
   gem 'hoe'
   gem 'rake'
   gem 'rake-compiler', '~> 0.9.3'
--- examples/my_lst.rb
+++ examples/my_lst.rb
@@ -4,6 +4,6 @@ puts 'My Local Apparent Sidereal Time'
 loop do
   eot.ajd = DateTime.now.to_time.utc.to_datetime.ajd
   puts "LST = #{ eot.string_time(((eot.tl_aries * Eot::R2D) + -88.75) / 15.0) }"
-  dur = (1 - 0.0027379093499 / 1.002739093499) / 1.0027379093499
-  sleep dur
+  # the number is syncing my machine. machine may vary in this.
+  sleep 0.99613381875
 end
