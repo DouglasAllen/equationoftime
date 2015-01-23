a2c331585d2fcd0aed9ed8bb0a85d86d488899ea
Douglas Allen <kb9agt@gmail.com>
2015-01-22 22:05:53 -0600

    fixed an example

--- examples/cal2jd.rb
+++ examples/cal2jd.rb
@@ -1,10 +1,10 @@
 require 'celes'
 require 'date'
+
 p d = DateTime.now.to_time.utc.to_datetime
-djm0, djm = Celes::cal2jd(d.year, d.month, d.day + d.day_fraction)
+djm0, djm = Celes::cal2jd(d.year, d.month, d.day)
 p djm0
 p djm
-p djm + djm0
-p d.ajd.to_f
-p d.day_fraction.to_f
-p (d.ajd - d.day_fraction).to_f
+p djm + djm0 + d.day_fraction.to_f
+# compare to inverse
+p DateTime.jd(0.5 + djm + djm0 + d.day_fraction.to_f)
