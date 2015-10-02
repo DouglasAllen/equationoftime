d84f77a2fcc9fd13989109ccf26e34e3cc1d8e7c
DouglasAllen <kb9agt@gmail.com>
2015-10-02 17:06:00 -0500

    updated ruby version

--- ext/helio/extconf.rb
+++ ext/helio/extconf.rb
@@ -1,12 +1,12 @@
 require 'mkmf'
 extension_name = 'helio/helio'
 
-LIBDIR      = RbConfig::CONFIG['libdir']
-INCLUDEDIR  = RbConfig::CONFIG['includedir']
+p LIBDIR      = RbConfig::CONFIG['libdir']
+p INCLUDEDIR  = RbConfig::CONFIG['includedir']
 
 HEADERDIRS = File.expand_path('../', __FILE__)
 
-LIBDIRS = File.expand_path('../lib', __FILE__)
+p LIBDIRS = File.expand_path('../lib', __FILE__)
 
 unless find_header('ruby.h', INCLUDEDIR)
   abort "ruby.h can't be found."
