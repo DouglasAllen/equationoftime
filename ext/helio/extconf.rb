bd3eeb72fa04bd75985133b1dd0e878b15493f9b
DouglasAllen <kb9agt@gmail.com>
2015-10-02 00:19:55 -0500

    including the sofa lib now

--- ext/helio/extconf.rb
+++ ext/helio/extconf.rb
@@ -16,7 +16,7 @@ HEADER_DIRS = [
 
   # Finally fall back to /usr
   #'/usr/include',
-  #'../',
+  '../',
 ]
 
 LIB_DIRS = [
@@ -31,17 +31,17 @@ LIB_DIRS = [
 
   # Finally fall back to /usr
   #'/usr/lib',
-  #'../,
+  '../',
 ]
 
 unless find_header('ruby.h')
   abort "ruby.h can't be found."
 end
 unless find_header('sofam.h')
-  abort "libsofa_c is missing.  please install libsofa_c"
+  abort "libsofa_c is missing.  please install libsofa-c-dev"
 end
-unless find_library('sofa_c', 'iauCal2jd')
-  abort "libsofa_c is missing.  please install libsofa_c"
+unless find_library('sofa_c', 'iauFapl03')
+  abort "libsofa_c is missing.  please install libsofa-c0"
 end
 dir_config('sofa_c', HEADER_DIRS, LIB_DIRS)
 dir_config(extension_name)
