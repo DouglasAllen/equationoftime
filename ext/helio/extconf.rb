bb045dc83da559389b2c77447826a1dbc74f7596
DouglasAllen <kb9agt@gmail.com>
2015-10-02 17:41:24 -0500

    updated ruby version

--- ext/helio/extconf.rb
+++ ext/helio/extconf.rb
@@ -1,12 +1,16 @@
 require 'mkmf'
 extension_name = 'helio/helio'
 
-p LIBDIR      = RbConfig::CONFIG['libdir']
-p INCLUDEDIR  = RbConfig::CONFIG['includedir']
+LIBDIR      = RbConfig::CONFIG['libdir']
+INCLUDEDIR  = RbConfig::CONFIG['includedir']
 
-HEADERDIRS = File.expand_path('../include', __FILE__)
+HEADERDIRS = [INCLUDEDIR]
 
-p LIBDIRS = File.expand_path('../lib', __FILE__)
+# setup constant that is equal to that of the file path that holds that static libraries 
+# that will need to be compiled against
+LIBDIRS = [LIBDIR, File.expand_path(File.join(File.dirname(__FILE__), "lib"))]
+# array of all libraries that the C extension should be compiled against
+libs = ['-lsofa_c']
 
 unless find_header('ruby.h', INCLUDEDIR)
   abort "ruby.h can't be found."
@@ -16,13 +20,19 @@ unless find_header('sofa.h', HEADERDIRS)
   abort "sofa.h and sofam.h are missing.  please install libsofa-c-dev"
 end
 
-LIBS = 'sofa_c'
-FUNC = 'iauFalp03'
-unless find_library(LIBS, FUNC, LIBDIRS)
-  abort "libsofa_c is missing.  please install libsofa-c.a"
-end
+# LIBS = 'sofa_c'
+# FUNC = 'iauFalp03'
+# unless find_library(LIBS, FUNC, LIBDIRS)
+#   abort "libsofa_c is missing.  please install libsofa-c.a"
+# end
 
 dir_config('sofa_c', HEADERDIRS, LIBDIRS)
+
+# iterate though the libs array, and append them to the $LOCAL_LIBS array used for 
+# the makefile creation
+libs.each do |lib|
+  $LOCAL_LIBS << "#{lib} "
+end
 
 dir_config(extension_name)
 create_makefile(extension_name)
