20cb87ee4bc3128d39c27c84d3029c108ad4ca58
Douglas Allen <kb9agt@gmail.com>
2015-10-28 19:02:41 -0500

    checking example compat

--- examples/eot_from_noon.rb
+++ examples/eot_from_noon.rb
@@ -1,14 +1,16 @@
 require 'eot'
 eot = Eot.new
-puts "My Longitude: #{eot.longitude = -75.324}"
-puts "The current UTC noon as ajd: #{eot.ajd}"
-puts "My mean local noon as ajd  : #{eot.mean_local_noon_jd}"
-puts "My true local noon as ajd  : #{eot.local_noon_dt.ajd.to_f}"
-eot_jd = eot.mean_local_noon_jd - eot.local_noon_dt.ajd
+puts "My Longitude              : #{eot.longitude = -75.324}"
+puts "Longitude / 360           : #{eot.longitude / 360}"
+eot.new_date(DateTime.now.to_time.utc.to_date.to_s)
+puts "The current UTC noon as jd: #{eot.jd}"
+puts "My mean noon UTC as jd    : #{eot.mean_local_noon_jd}"
+puts "My solar noon UTC as jd   : #{eot.solar_noon_jd}"
+eot_jd = eot.mean_local_noon_jd - eot.solar_noon_dt.ajd
 puts "Note: ajd and jd decimals only go so far and so slight difference here"
-puts "and obviously some rounding occurs when looking at the second ajd above."
-puts "mean - true: #{e1 = eot_jd}"
-puts "eot.eot_jd : #{e2 = eot.eot_jd}"
-puts "The differnce: #{e1 - e2}"
-puts "which in minutes is: #{1440.0 * (e1 - e2)}"
-puts "and in arc minutes is: #{15.0 * 1440.0 * (e1 - e2)}"
+puts "and obviously some rounding occurs when looking at the second jd above."
+puts "mean - solar              : #{e1 = eot_jd}"
+puts "eot.eot_jd                : #{e2 = eot.eot_jd}"
+puts "The differnce in degrees  : #{(e1 - e2) * 360}"
+puts "which in arc minutes is   : #{(e1 - e2) * 360 * 60}"
+puts "and in arc seconds is     : #{(e1 - e2) * 360 * 3600}"
