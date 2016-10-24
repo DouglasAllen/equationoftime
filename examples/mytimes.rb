d259bfefe285468d612130f75867b19b7f608666
Douglas <kb9agt@gmail.com>
2016-10-23 21:10:53 -0500

    rewoking the C code

--- examples/mytimes.rb
+++ examples/mytimes.rb
@@ -19,6 +19,27 @@ require_relative '../lib/eot'
 p @eot.sunrise_dt
 p @eot.local_noon_dt
 p @eot.sunset_dt
+p @eot.sunrise_dt.rfc3339
+p @eot.local_noon_dt.rfc3339
+p @eot.sunset_dt.rfc3339
+p @eot.sunrise_dt.iso8601
+p @eot.local_noon_dt.iso8601
+p @eot.sunset_dt.iso8601
+p @eot.sunrise_dt.xmlschema
+p @eot.local_noon_dt.xmlschema
+p @eot.sunset_dt.xmlschema
+p @eot.sunrise_dt.jisx0301
+p @eot.local_noon_dt.jisx0301
+p @eot.sunset_dt.jisx0301
+p @eot.sunrise_dt.rfc2822
+p @eot.local_noon_dt.rfc2822
+p @eot.sunset_dt.rfc2822
+p @eot.sunrise_dt.rfc822
+p @eot.local_noon_dt.rfc822
+p @eot.sunset_dt.rfc822
+p @eot.sunrise_dt.to_time.rfc822
+p @eot.local_noon_dt.to_time.rfc822
+p @eot.sunset_dt.to_time.rfc822
 
 if __FILE__ == $PROGRAM_NAME
 
